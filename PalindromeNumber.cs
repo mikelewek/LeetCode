fd80ab70142c55438eac1ce0c5da79baeeccc5bf
mikelewek <32617630+mikelewek@users.noreply.github.com>
2020-01-02 02:11:18 -0500

    update palindrome formatting

--- PalindromeNumber.cs
+++ PalindromeNumber.cs
@@ -1,18 +1,18 @@
 public class Solution {
     public bool IsPalindrome(int x) {
         string reverseX = string.Empty;
-            string stringX = x.ToString();
+        string stringX = x.ToString();
 
-            for (var i = 1; i <= stringX.Length; i++)
-            {
-                reverseX += stringX[stringX.Length - i];
-            }
+        for (var i = 1; i <= stringX.Length; i++)
+        {
+            reverseX += stringX[stringX.Length - i];
+        }
 
-            if (stringX == reverseX)
-            {
-                return true;
-            }
+        if (stringX == reverseX)
+        {
+            return true;
+        }
 
-            return false;
+        return false;
     }
 }
